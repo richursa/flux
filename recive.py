e2db6f5ae1c62625d9ac73a72e38ac6b58473a3f
richu <richursa@gmail.com>
2019-02-07 19:21:30 +0530

    added files

---
+++ recive.py
@@ -0,0 +1,8 @@
+import pika
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+def callback(ch , method,properties,body):
+	print "recived " , body
+channel.basic_consume(callback,queue= 'hello',no_ack = True)
+channel.start_consuming()
