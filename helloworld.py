f9a300a168afeb992b45a10acd06077846d57eb8
richu <richursa@gmail.com>
2019-02-07 21:27:14 +0530

    added comments

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,12 @@
-#rabbitmq
-import pika
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-channel.queue_declare(queue='hello')
-channel.basic_publish(exchange='' ,routing_key = 'hello', body = 'Hello World!')
-print "hello world sent"
-connection.close()
+import pika             #rabbitmq library
+rabbitMqExchangeServer = 'localhost'        #rabbitmq exchange server for development purpose only
+connection = pika.BlockingConnection(pika.ConnectionParameters(rabbitMqExchangeServer))        #create a connection object which is should be connected to rabbitmq server
+channel = connection.channel()      #create a channel object
+rabbitMqQueue = 'hello'     #sample queue name
+channel.queue_declare(queue=rabbitMqQueue)        #define which rabbitMq queue to use 
+#always true loop for dev purpose
+while(1):
+    inputString = input()
+    channel.basic_publish(exchange='' ,routing_key = rabbitMqQueue, body = inputString)     #exchange server '' ? send a msg to queue 
+    print inputString , "sent"
+connection.close()      #connection should be closed to flush all msgs from buffer to queue
